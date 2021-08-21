98ab8eac4476d2a42fcb5c26d70c73c44f26c6a7
d3gerv <60596339+d3gerv@users.noreply.github.com>
2021-08-21 20:58:34 +0800

    Frontend+BackEnd

---
+++ app/src/main/java/com/example/capstone1/new_measurements.java
@@ -0,0 +1,14 @@
+package com.example.capstone1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class new_measurements extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_measurements);
+    }
+}
