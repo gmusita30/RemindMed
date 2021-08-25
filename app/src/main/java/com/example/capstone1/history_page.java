3bf459fd3dfe6a0bcd6b54f8bc4d042b9321de4c
arahnicolle <arahnicolle.tolentino.iics@ust.edu.ph>
2021-08-25 17:28:32 +0800

    new

--- app/src/main/java/com/example/capstone1/history_page.java
+++ app/src/main/java/com/example/capstone1/history_page.java
@@ -3,12 +3,17 @@ package com.example.capstone1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class history_page extends AppCompatActivity {
+
+    public TextView history;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history_page);
+
+
     }
 }
