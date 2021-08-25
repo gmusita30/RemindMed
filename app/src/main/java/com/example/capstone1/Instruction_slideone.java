81c5e1e3be8f7e9c48ae559db0c04d8eacf0b5c2
arahnicolle <arahnicolle.tolentino.iics@ust.edu.ph>
2021-08-25 17:04:51 +0800

    new

---
+++ app/src/main/java/com/example/capstone1/Instruction_slideone.java
@@ -0,0 +1,14 @@
+package com.example.capstone1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Instruction_slideone extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instruction_slideone);
+    }
+}
