c828c0f37c8f195496c676ac2f3cfd89b082d6d1
arahnicolle <arahnicolle.tolentino.iics@ust.edu.ph>
2021-08-26 15:05:35 +0800

    fixed icons and images

--- app/src/main/java/com/example/capstone1/recover.java
+++ app/src/main/java/com/example/capstone1/recover.java
@@ -10,6 +10,7 @@ public class recover extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recover);
+        //
         
     }
 }
